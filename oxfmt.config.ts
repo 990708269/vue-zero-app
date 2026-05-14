01528acd0eb47f4cc03a3b86f2f378504c1a2980
zwl <z18610655402@163.com>
2026-05-14 21:42:44 +0800

    test husky

---
+++ oxfmt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+  printWidth: 80,
+  tabWidth: 2,
+  // 使用单引号
+  singleQuote: true,
+  // 不要分号
+  semi: false,
+  // 在对象字面量中打印括号
+})
